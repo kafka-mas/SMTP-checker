0080ebaa193668c1d98cb1f5b4e97dda19198346
Kafka <kafka_mas@disroot.org>
2026-05-26 12:17:22 +0300

    Setup reciever

--- main.go
+++ main.go
@@ -19,26 +19,26 @@ func main() {
 
 	// externalMail := os.Getenv("SEND_EXT")
 	// externalPass := os.Getenv("PASS_EXT")
-	// internalMail := os.Getenv("SEND_IN")
+	internalMail := os.Getenv("SEND_IN")
 	internalPass := os.Getenv("PASS_IN")
-	// bolidMailer := sender.NewMailSender("checkmail", internalPass, "mail-serv.bolid.ru")
-	// bolidMailer := sender.NewMailSender(internalMail, internalPass, "mail-serv.bolid.ru",
+	// bbbMailer := sender.NewMailSender("checkmail", internalPass, "mail-serv.bbb.ru")
+	// bbbMailer := sender.NewMailSender(internalMail, internalPass, "mail-serv.bbb.ru",
 	// 	sender.WithCustomAuth(),
 	// 	sender.WithCustomUsername("checkmail"),
 	// 	sender.WithCustomPort(25),
 	// 	sender.WithNoTLS(),
-	// 	// sender.WithHELO("mail-serv.bolid.ru"),
+	// 	// sender.WithHELO("mail-serv.bbb.ru"),
 	// )
 	//
 	// mailMailer := sender.NewMailSender(externalMail, externalPass, "smtp.mail.ru")
 	//
-	// var msgBolidID []string
-	// msgBolidID, err := bolidMailer.Send(externalMail, "Test mail", "Message")
+	// var msgbbbID []string
+	// msgbbbID, err := bbbMailer.Send(externalMail, "Test mail", "Message")
 	// if err != nil {
 	// 	log.Fatalf("error send message: %s", err)
 	// }
 	//
-	// fmt.Println("Sent message ID:", msgBolidID[0])
+	// fmt.Println("Sent message ID:", msgbbbID[0])
 	//
 	// var msgMailID []string
 	// msgMailID, err = mailMailer.Send(internalMail, "Test mail", "Message")
@@ -48,12 +48,13 @@ func main() {
 	//
 	// fmt.Println("Sent message ID:", msgMailID[0])
 
-
 	// var msg []string = []string{"123"}
 	// err = receiver.WaitForEmail(externalMail, externalPass, "INBOX", msg[0])
-	imap := receiver.NewMailReceiver("", internalPass, "")
-	err := imap.Receive("INBOX","")
+	imap := receiver.NewMailReceiver(internalMail, internalPass, "imap.gmail.com", receiver.SetTimeout(10))
+	isMailFind, err := imap.Receive("INBOX", "")
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
+	
+	fmt.Println(isMailFind)
 }
--- receiver/receiver.go
+++ receiver/receiver.go
@@ -2,12 +2,14 @@ package receiver
 
 import (
 	"fmt"
-	// "github.com/emersion/go-imap/v2"
+	"time"
+
+	"github.com/emersion/go-imap/v2"
 	"github.com/emersion/go-imap/v2/imapclient"
 )
 
 type MailReceiver interface {
-	Receive(targetInbox, messageID string) error
+	Receive(targetInbox, messageID string) (bool, error)
 }
 
 type mailReceiver struct {
@@ -23,57 +25,113 @@ func NewMailReceiver(login, password, imapServer string, opts ...IMAPoptions) Ma
 		password:   password,
 		imapServer: imapServer,
 		opts: options{
-			useTLS: false,
-			port:   143,
+			useTLS:         true,
+			port:           993,
+			customUsername: "",
+			timeout:        time.Duration(30 * time.Second),
 		},
 	}
+
+	for _, opt := range opts {
+		opt(&mr.opts)
+	}
+
 	return mr
 }
 
-type IMAPoptions func(*mailReceiver)
+type IMAPoptions func(*options)
 
 type options struct {
 	port           int
 	useTLS         bool
 	customUsername string
+	timeout        time.Duration
 }
 
 func WithCustomPort(port int) IMAPoptions {
-	return func(mr *mailReceiver) {
-		if port >0 && port < 65535 {
-			mr.opts.port = port
+	return func(mr *options) {
+		if port > 0 && port < 65535 {
+			mr.port = port
 		}
 	}
 }
 
-func (m *mailReceiver) Receive(targetInbox, messageID string) error {
-	// Настройки IMAP для Mail.ru
+func WithNoTLS() IMAPoptions {
+	return func(o *options) { o.useTLS = false }
+}
+
+func WithCustomUsername(username string) IMAPoptions {
+	return func(o *options) { o.customUsername = username }
+}
+
+//Set polling time in seconds
+func SetTimeout(timeout time.Duration) IMAPoptions {
+	return func(o *options) { o.timeout = timeout * time.Second }
+}
+
+func (m *mailReceiver) Receive(targetInbox, messageID string) (bool, error) {
 	fmt.Println("Подключение к серверу...")
 	url := fmt.Sprintf("%v:%d", m.imapServer, m.opts.port)
 	fmt.Println(url)
-	client, err := imapclient.DialInsecure(url, nil)
+	client, err := func(url string) (*imapclient.Client, error) {
+		if m.opts.useTLS {
+			client, err := imapclient.DialTLS(url, nil)
+			return client, err
+		} else {
+			client, err := imapclient.DialInsecure(url, nil)
+			return client, err
+		}
+	}(url)
 	if err != nil {
-		return fmt.Errorf("failed to dial IMAP: %w", err)
+		return false, fmt.Errorf("failed to dial IMAP: %w", err)
 	}
 	defer client.Logout()
 	fmt.Println("Подключено к серверу")
 
 	// Аутентификация
+	if m.opts.customUsername != "" {
+		m.username = m.opts.customUsername
+	}
 	if err := client.Login(m.username, m.password).Wait(); err != nil {
-		return fmt.Errorf("failed to login: %w", err)
+		return false, fmt.Errorf("failed to login: %w", err)
 	}
 	fmt.Println("Аутентификация успешна")
 
 	mbox, err := client.Select(targetInbox, nil).Wait()
 	if err != nil {
-		return fmt.Errorf("failed to select inbox: %w", err)
+		return false, fmt.Errorf("failed to select inbox: %w", err)
 	}
 
 	fmt.Printf("Всего писем: %d\n", mbox.NumMessages)
 
 	if mbox.NumMessages == 0 {
-		return nil
+		return false, nil
 	}
 
-	return nil
+	// 2. Настройка критериев поиска по Header
+	criteria := &imap.SearchCriteria{
+		Header: []imap.SearchCriteriaHeaderField{
+			{
+				Key:   "Message-Id",
+				Value: messageID,
+			},
+		},
+	}
+
+	timeStart := time.Now()
+	for {
+		if time.Since(timeStart) >= m.opts.timeout {
+			return false, fmt.Errorf("error get message with ID %v", messageID)
+		}
+
+		searchRes, err := client.UIDSearch(criteria, nil).Wait()
+		if err != nil {
+			return false, err
+		}
+		uids := searchRes.AllUIDs()
+		if len(uids) != 0 {
+			return true, nil
+		}
+		time.Sleep(time.Duration(5) * time.Second)
+	}
 }
